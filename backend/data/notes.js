6fd3f0a17764e2bcd1f999bcb056c2e5f608700c
PIIOT22JUN5001 <arnav.sar@gmail.com>
2022-08-19 02:10:01 +0530

    created frontend and backend

---
+++ backend/data/notes.js
@@ -0,0 +1,15 @@
+const notes=[
+    {
+        _id: "1",
+        title: "Day 1 of college",
+        content: "made frnds",
+        category: "college",
+    },
+    {
+        _id: "2",
+        title: "Day 1 of school",
+        content: "met teachers",
+        category: "school",
+    },
+]
+module.exports=notes;
